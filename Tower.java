5326c466a0a3be30b437e7aca9cacdf9f1fe7824
Lydia Goldberg <goldberg.lydia@gmail.com>
2014-05-28 21:22:24 -0400

    added price to Tower

--- Tower.java
+++ Tower.java
@@ -3,19 +3,24 @@ public class Tower{
     private int damage;
     private int range;
     private int speed;
+    private int price;
 
     public Tower(){
-	//Standard Tower
-	damage = 1;
-	range = 3; //tiles
-	speed = 1;
+	    //Standard Tower
+	    damage = 1;
+	    range = 3; //tiles
+	    speed = 1;
     }
 
     public Tower(int d, int r, int s){
-	//Modified Tower
-	damage = d;
-	range = r;
-	speed = s;
+	    //Modified Tower
+	    damage = d;
+	    range = r;
+	    speed = s;
+    }
+    
+    public int getPrice() {
+        return price;
     }
 
 }
